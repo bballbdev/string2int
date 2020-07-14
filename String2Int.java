acad381670417d06aaa3491e270a238dc14524fb
bballbdev <bballbdev@hotmail.com>
2020-07-14 16:12:00 +0700

    add description comment

--- String2Int.java
+++ String2Int.java
@@ -11,8 +11,11 @@ public class String2Int {
         int answer = 0;
         for(int i = str.length()-1; i >= 0; i--){
             char letter = str.charAt(i);
+            // check if a letter is a alphabet or not.
             if(!(letter >= 'a' && letter <= 'z') && !(letter >= 'A' && letter <= 'Z')){
+                //letter - 48 --> do subtract ASCII value of number by 48 for real number value.
                 answer += (letter - 48) * Math.pow(10, power);
+                // increase exponent for multiple with next letter. 
                 power += 1;
             }
         }
